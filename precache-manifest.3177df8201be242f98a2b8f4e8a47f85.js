e47990070b5f0b5c1c15fdb2a60a89785a2bdb64
Aldi Vandiaz Maulana <aldivanzdiazmaulana@gmail.com>
2026-08-10 08:43:04 +0700

    Updates

--- precache-manifest.3177df8201be242f98a2b8f4e8a47f85.js
+++ precache-manifest.3177df8201be242f98a2b8f4e8a47f85.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b926fff54b3465ad746c1472073f1b1e",
+    "revision": "39e537df3014857c79f8cdf2137dc881",
     "url": "/index.html"
   },
   {
-    "revision": "2d729b543cc9e85ef1e5",
-    "url": "/static/css/main.5b7274c6.chunk.css"
+    "revision": "80fcc5a55dfac4d384d2",
+    "url": "/static/css/main.88daa00a.chunk.css"
   },
   {
     "revision": "81697d856e120d817a35",
     "url": "/static/js/2.fdbfc0be.chunk.js"
   },
   {
-    "revision": "2d729b543cc9e85ef1e5",
-    "url": "/static/js/main.8c18aa40.chunk.js"
+    "revision": "80fcc5a55dfac4d384d2",
+    "url": "/static/js/main.f8a8a403.chunk.js"
   },
   {
     "revision": "f4163cc1361175add5e6",
